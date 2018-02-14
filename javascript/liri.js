fefe5fa8f6c648c565b0c8ee83abcd8e223d37e7
Tom LaChance <guitarlanguageseries@gmail.com>
2018-02-13 18:32:34 -0600

    installed required npm packages, created new twitter account

--- javascript/liri.js
+++ javascript/liri.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 //9. Add the code required to import the keys.js file and store it in a variable
+var fs = require("fs");
 var keys = require("./keys");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
