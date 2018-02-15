7274a28af4c7d610a0b34452cc0c15202d910af4
Tom LaChance <guitarlanguageseries@gmail.com>
2018-02-15 16:01:30 -0600

    started a control flow statement that takes in argv[2] parameters.  my-tweets now includes the created_on value

--- javascript/liri.js
+++ javascript/liri.js
@@ -1,6 +1,4 @@
-
 require("dotenv").config();
-//9. Add the code required to import the keys.js file and store it in a variable
 var fs = require("fs");
 var keys = require("./keys");
 var Spotify = require('node-spotify-api');
@@ -9,33 +7,62 @@ var Twitter = require('twitter');
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-//twitter logic
-var params = {mott_jenkins: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-      var count = 0;
-      for (var i = 0; i < tweets.length; i++) {
-        count++;
-        console.log(`tweet number ${count}: ${tweets[i].text}`);
-      }
 
-  }
-});
+//function to determine the argv and which way to go
+// function argControl() {
+//     if (process.argv[2] === "my-tweets") {
+//         twitterLogic();
+//     } else if (process.argv[2] === "spotify-this-song") {
+//         //some function
+//     } else if (process.argv[2] === "movie-this") {
+//         //some function
+//     } else if (process.argv[2] === "do-what-it-says") {
+//         //some function
+//     } else {
+//         return;
+//     }
+//
+// };
+
+function twitterLogic() {
+    //straight from api documentation
+    //twitter user parameter object
+    var params = {
+        mott_jenkins: 'nodejs'
+    };
+
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            var count = 0;
+            for (var i = 0; i < tweets.length; i++) {
+                count++;
+                var created = tweets[i].created_at.slice(0, 30); //wanted to
+                //cut out the 0000, but that would have meant losing the year.
+                console.log(`tweet number ${count}: ${tweets[i].text} | created on: ${created}`);
+            }
+        }
+    });
+};
+twitterLogic();
+
 //spotify logic straight from their documentation
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-} else {
-    for (var s = 0; s < data.length; s++){
+function spotifyTune() {
+    spotify.search({
+        type: 'track',
+        query: 'All the Small Things'
+    }, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        } else {
+            for (var s = 0; s < data.length; s++) {
 
-    }
-}
+            }
+        }
 
-console.log(JSON.stringify(data.tracks.items[0].album.name, null, 2));
-});
+        console.log(JSON.stringify(data.tracks.items[0].album.name, null, 2));
+    });
 
-
-
+};
 
 
 
